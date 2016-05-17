2b66cc25256193ce45e5116dd201ad6ea3552518
Jeremy Neiman <docmarionum1@gmail.com>
2016-05-17 17:16:55 -0400

    create query function and demo

--- demo/index.js
+++ demo/index.js
@@ -1,5 +1,57 @@
 angular
 .module('DemoApp', ['WifiMapping'])
 .controller('DemoCtrl', function($scope, wifiMappingAPI) {
-    $scope.text = wifiMappingAPI.test();
+    $scope.params = {
+        page_size: 5,
+        page: 0,
+        acc: '',
+        altitude: '',
+        startdate: '5/10/2016',
+        enddate: '',
+        device_mac: '',
+        app_version: '',
+        droid_version: '',
+        bssid: '',
+        ssid: 'nyu',
+        caps: '',
+        level: '',
+        freq: ''
+    };
+
+    $scope.columns = {
+        lat: false,
+        lng: false,
+        acc: false,
+        altitude: false,
+        time: false,
+        device_mac: false,
+        app_version: false,
+        droid_version: false,
+        device_model: false,
+        ssid: true,
+        bssid: false,
+        caps: false,
+        level: false,
+        freq: false
+    };
+
+    $scope.execute = function() {
+        $scope.text = 'Pending...';
+        
+        var columns = [];
+        for (var col in $scope.columns) {
+            if ($scope.columns[col]) {
+                columns.push(col);
+            }
+        }
+        var params = {columns: columns};
+        for (param in $scope.params) {
+            if ($scope.params[param]) {
+                params[param] = $scope.params[param];
+            }
+        }
+        wifiMappingAPI.query(params).then(function(res) {
+            $scope.text = res;
+        });
+    }
 });
